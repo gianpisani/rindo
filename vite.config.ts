4c349aa76f55dd6b2da60caf08c22e9ad45a03ed
gianpisani <gpisani9@gmail.com>
2026-02-13 12:29:17 -0300

    chore: build deploy

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig(({ mode }) => ({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
         runtimeCaching: [
           {
